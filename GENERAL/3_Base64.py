23ace1e1c5b24d89741e5dee5f9a972af90d52cf
kenny-420 <111099583+kenny-420@users.noreply.github.com>
2022-08-12 11:43:43 +1000

    Update 3_Base64.py

--- GENERAL/3_Base64.py
+++ GENERAL/3_Base64.py
@@ -1,2 +1,2 @@
 from base64 import b64encode
-print('FLAG:', b64encode(bytes.fromhex('72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf')).decode())
+print('FLAG:', b64encode(bytes.fromhex('72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf')).decode()
