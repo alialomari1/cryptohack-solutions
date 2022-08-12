3ba7363f21f4018204a59d2abe822e8a01cae7ba
kenny-420 <111099583+kenny-420@users.noreply.github.com>
2022-08-12 14:55:42 +1000

    Create 10_Lemur_XOR.py

---
+++ GENERAL/10_Lemur_XOR.py
@@ -0,0 +1,5 @@
+from PIL import Image
+from pwn import xor
+lemur = Image.open('lemur.png')
+flag = Image.open('flag.png')
+Image.frombytes(flag.mode, flag.size, xor(lemur.tobytes(), flag.tobytes())).save('solve.png')
