98dbab3c50ee1f70111ffb6028c40aaa5b8a7942
kenny-420 <111099583+kenny-420@users.noreply.github.com>
2022-08-12 15:53:42 +1000

    Update 07_XOR_Properties.py

--- GENERAL/07_XOR_Properties.py
+++ GENERAL/07_XOR_Properties.py
@@ -2,4 +2,4 @@ key1 = 0xa6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313
 key2 = key1 ^ 0x37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e
 key3 = key2 ^ 0xc1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1
 flag = key1 ^ key2 ^ key3 ^ 0x04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf
-print('FLAG:', bytes.fromhex(hex(flag)[2:]).decode())
+print(bytes.fromhex(hex(flag)[2:]).decode())
