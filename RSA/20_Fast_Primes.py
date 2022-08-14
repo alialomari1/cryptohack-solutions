612b802a01f8aa15d073726641f415c2fa3ea0a3
kenny-420 <111099583+kenny-420@users.noreply.github.com>
2022-08-15 08:59:13 +1000

    Create 20_Fast_Primes.py

---
+++ RSA/20_Fast_Primes.py
@@ -0,0 +1,12 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+e = 65537
+c = 0x249d72cd1d287b1a15a3881f2bff5788bc4bf62c789f2df44d88aae805b54c9a94b8944c0ba798f70062b66160fee312b98879f1dd5d17b33095feb3c5830d28
+p = 51894141255108267693828471848483688186015845988173648228318286999011443419469
+q = 77342270837753916396402614215980760127245056504361515489809293852222206596161
+n = p*q
+d = pow(e, -1, (p-1)*(q-1))
+key = RSA.construct((n, e, d))
+cipher = PKCS1_OAEP.new(key)
+print(cipher.decrypt(bytes.fromhex(hex(c)[2:])).decode())
